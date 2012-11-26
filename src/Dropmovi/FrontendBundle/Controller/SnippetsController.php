3ef9915f93df32d27ee5c373fa41253272323edd
Jesus Diaz <gbuuss@gmail.com>
2012-11-26 02:01:09 -0300

    modify the name of snippet newPublications

--- src/Dropmovi/FrontendBundle/Controller/SnippetsController.php
+++ src/Dropmovi/FrontendBundle/Controller/SnippetsController.php
@@ -11,9 +11,9 @@ class SnippetsController extends Controller {
         return $this->render('DropmoviFrontendBundle:Snippets:listPublications.html.twig', array('publications' => $publications));
     }
 
-    public function newsPublicaitonsAction(){
+    public function newPublicationsAction(){
     	$publications = $this->get('publication.manager')->getNewPublications();
-    	return $this->render('DropmoviFrontendBundle:Snippets:newsPublications.html.twig', array('publications' => $publications));
+    	return $this->render('DropmoviFrontendBundle:Snippets:newPublications.html.twig', array('publications' => $publications));
     }
 
     public function authorPublicationsAction($idUser, $idPublication){
