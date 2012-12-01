041b1e69ed21785891c178c246d48ac831553e04
Jesus Diaz <gbuuss@gmail.com>
2012-12-01 01:12:42 -0300

    modify bundles

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit937e5ec629f2a06a1a767643a8693f2f::getLoader();
+return ComposerAutoloaderInit3d9fe1138cdc9043f4ecea87f2aecd95::getLoader();
